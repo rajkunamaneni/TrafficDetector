aaa2fd7228c76097d52315a1448c505d279152bf
ComradeConejo <georgezaets@gmail.com>
2023-12-11 18:57:22 -0800

    wireshark dependencies

--- start.sh
+++ start.sh
@@ -12,6 +12,14 @@ apt install -y wireshark
 
 echo 'Installation of Wireshark complete'
 
+echo 'Attempting to install Wireshark Dependencies'
+
+apt install -y tshark
+
+apt install -y rpm
+
+echo 'Installation of Wireshark Dependencies complete'
+
 echo 'Attempting to install Snort'
 
 apt install -y snort
