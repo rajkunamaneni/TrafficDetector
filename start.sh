283189133883426c23515987e0a56acc2208d19e
ComradeConejo <georgezaets@gmail.com>
2023-12-12 21:11:34 -0800

    Start.sh update

--- start.sh
+++ start.sh
@@ -40,5 +40,29 @@ apt install -y fping
 
 echo 'Installation of Fping complete'
 
+echo 'Attempting to install Python3'
+
+apt install -y python3
+
+echo 'Installation of Python3 complete'
+
+echo 'Attempting to install Python3 Pip'
+
+apt install -y python3-pip
+
+echo 'Installation of Python3 Pip complete'
+
+echo 'Attempting to install boto3'
+
+pip3 install boto3
+
+echo 'Installation of boto3 complete'
+
+echo 'Attempting to install AWS CLI'
+
+pip3 install awscli
+
+echo 'Installation of AWS CLI complete'
+
 echo 'Full Installation of TrafficDetector complete!'
 
